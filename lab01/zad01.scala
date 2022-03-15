eaba55ecb762bcfa4a17e390cc924804e287f4df
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-15 15:36:27 +0100

    Laboratoria 1 - zadanie 1

--- lab01/zad01.scala
+++ lab01/zad01.scala
@@ -2,26 +2,14 @@ object zad01 {
    def obramuj(napis: String): String = {
       val linijki: Array[String] = napis.split('\n')
       val najdluzsza: Int = linijki.maxBy(s => s.length).length
-      var wynik: String = ""
-      for (i <- 1 to najdluzsza + 4) {
-         wynik += "*"
-      }
-      wynik += "\n"
-      linijki.foreach(element =>
-         wynik += "* " + element
-         for (j <- 1 to (najdluzsza - element.length)) {
-            wynik += " "
-         }
-         wynik += " *\n"
-      )
-      for (i <- 1 to najdluzsza + 4) {
-         wynik += "*"
-      }
-      return wynik
+      var wynik: String = "*" * (najdluzsza + 4) + '\n'
+      linijki.map(s => "* " + s + " " * (najdluzsza - s.length) + " *").foreach(s => wynik += s + '\n')
+      wynik += "*" * (najdluzsza + 4)
+      wynik
    }
 
    def main(args: Array[String]): Unit = {
-      val dane: String = "Przykladowy tekst\nzwierajacy\ntrzy linijki"
+      val dane: String = "Przykladowy tekst\nzawierajacy\nwiecej niz\n3 linijki."
       println(obramuj(dane))
    }
 }
