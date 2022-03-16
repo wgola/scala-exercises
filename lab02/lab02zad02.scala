749beab3e91798869e23e847a035a6b2f9e16bee
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-16 10:53:50 +0100

    Laboratoria 2 - zadanie 2

---
+++ lab02/lab02zad02.scala
@@ -0,0 +1,10 @@
+@annotation.tailrec
+def nwd(a: Int, b: Int): Int = {
+    if (a % b == 0) b
+    else nwd(b, a % b)
+}
+
+@main def lab2zad2(a: Int, b: Int): Unit = {
+    println("Podane argumenty: " + a + ", " + b)
+    println("Największy wspólny dzielnik: " + nwd(a, b))
+}
