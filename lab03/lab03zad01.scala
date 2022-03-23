883c187aa28b073cd0a45abdc53f0e656de287dc
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-23 10:31:07 +0100

    Laboratoria 3 - zadanie 1

--- lab03/lab03zad01.scala
+++ lab03/lab03zad01.scala
@@ -1,3 +1,12 @@
+import scala.annotation.tailrec
+
+@tailrec
+def reverse(str: String, akum: String = ""): String = {
+    if (str.length == 0) akum
+    else reverse(str.tail, str.head + akum)
+}
+
+
 @main def lab3zad1(): Unit = {
-   println("test")
+   println(reverse("ala ma kota"))
 }
