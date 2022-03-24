efddb62b0a43e57eabeac194d20ead1acd35d842
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-24 10:55:43 +0100

    Laboratoria 3 - zadanie 4

--- lab03/lab03zad04.scala
+++ lab03/lab03zad04.scala
@@ -1,15 +1,38 @@
+import scala.annotation.tailrec
+
 def tasuj(l1: List[Int], l2: List[Int]): List[Int] = {
-    def pomocnicza(lista1: List[Int], lista2: List[Int], akum: List[Int] = List(), poprzedni: Int = 0): List[Int] = {
-        if (lista1.length == 0 && lista2.length == 0) akum
-        else if (lista1.head < lista2.head && lista1.head != poprzedni) pomocnicza(lista1.tail, lista2, akum :+ lista1.head, lista1.head)
-        else if (lista2.head < lista1.head && lista2.head != poprzedni) pomocnicza(lista1, lista2.tail, akum :+ lista2.head, lista2.head)
-        else pomocnicza(lista1.tail, lista2.tail, akum :+ lista1.head :+ lista2.head)
+    @tailrec    
+    def pomocnicza(lista1: List[Int], lista2: List[Int], wynik: List[Int] = List(), poprzedni: Int = 0, licznik: Int = 0): List[Int] = {
+        if (licznik == 0) {
+            if (lista1.isEmpty && lista2.isEmpty) wynik
+            else if (lista1.nonEmpty && lista2.isEmpty) pomocnicza(lista1.tail, lista2, wynik :+ lista1.head, lista1.head, licznik + 1)
+            else if (lista1.isEmpty && lista2.nonEmpty) pomocnicza(lista1, lista2.tail, wynik :+ lista2.head, lista2.head, licznik + 1)
+            else if (lista1.head < lista2.head) pomocnicza(lista1.tail, lista2, wynik :+ lista1.head, lista1.head, licznik + 1)
+            else pomocnicza(lista1, lista2.tail, wynik :+ lista2.head, lista2.head, licznik + 1)
+        }
+        else {
+            if (lista1.isEmpty && lista2.isEmpty) wynik
+            else if (lista1.nonEmpty && lista2.isEmpty) {
+                if (lista1.head != poprzedni) pomocnicza(lista1.tail, lista2, wynik :+ lista1.head, lista1.head, licznik + 1)
+                else pomocnicza(lista1.tail, lista2, wynik, poprzedni, licznik + 1)
+            }
+            else if (lista1.isEmpty && lista2.nonEmpty) {
+                if (lista2.head != poprzedni) pomocnicza(lista1, lista2.tail, wynik :+ lista2.head, lista2.head, licznik + 1)
+                else pomocnicza(lista1, lista2.tail, wynik, poprzedni, licznik + 1)
+            }
+            else if (lista1.head < lista2.head) {
+                if (lista1.head != poprzedni) pomocnicza(lista1.tail, lista2, wynik :+ lista1.head, lista1.head, licznik + 1)
+                else pomocnicza(lista1.tail, lista2, wynik, poprzedni, licznik + 1)
+            }
+            else {
+                if (lista2.head != poprzedni) pomocnicza(lista1, lista2.tail, wynik :+ lista2.head, lista2.head, licznik + 1)
+                else pomocnicza(lista1, lista2.tail, wynik, poprzedni, licznik + 1)
+            }
+        }
     }
     pomocnicza(l1, l2)
 }
 
-
-
 @main def lab3zad4(): Unit = {
-   println(tasuj(List(1, 2, 3), List(4, 5, 6)))
+   println(tasuj(List(2, 4, 3, 5), List(1, 2, 2, 3, 1, 5)))
 }
