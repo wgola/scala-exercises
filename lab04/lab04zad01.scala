443a01c6e55011e586eceaf3ee3fa24cc71573c3
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-30 10:46:47 +0200

    Laboratoria 4 - zadanie 1

--- lab04/lab04zad01.scala
+++ lab04/lab04zad01.scala
@@ -1,3 +1,19 @@
+import scala.annotation.tailrec
+
+def sumuj(l: List[Option[Double]]): Option[Double] = {
+    @tailrec
+    def helper(l: List[Option[Double]], akum: Option[Double]): Option[Double] = {
+        l match {
+            case head :: tail => {
+                if (head.getOrElse(0.0) > 0) helper(tail, Some(akum.getOrElse(0.0) + head.get))
+                else helper(tail, akum) 
+            }
+            case _ => akum
+        }
+    }
+    helper(l, None)
+} 
+
 @main def lab4zad1(): Unit = {
-    println("test")
+    println(sumuj(List(Some(2.0), Some(4.0), Some(-3.0), None, Some(-3.0), None, Some(1.0))))
 }
