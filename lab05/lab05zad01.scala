1f8b583ef2349bbb242ce4719ee946c5adc9ad0a
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-06 10:38:51 +0200

    Laboratoria 5 - zadanie 1

--- lab05/lab05zad01.scala
+++ lab05/lab05zad01.scala
@@ -1,3 +1,22 @@
+import scala.annotation.tailrec
+
+def oczysc[A](l: List[A]): List[A] = {
+    @tailrec
+    def helper[A](lista: List[A], poprzedni: Option[A] = None, wynik: List[A] = List()): List[A] = {
+        lista match {
+            case element :: reszta => {
+                if (poprzedni.nonEmpty) {
+                    if (element != poprzedni.get) helper(reszta, Some(element), wynik :+ element)
+                    else helper(reszta, poprzedni, wynik)
+                }
+                else helper(reszta, Some(element), wynik :+ element)
+            }
+            case _ => wynik
+        }
+    }
+    helper(l)
+} 
+
 @main def lab5zad1(): Unit = {
-    println(5)
+    println(oczysc(List(1, 1, 2, 4, 4, 4, 1, 3)))
 }
