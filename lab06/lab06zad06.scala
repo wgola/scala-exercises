4ca098dc2ec58419570b9efee990b8b98d276544
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-13 12:58:57 +0200

    Laboratoria 6 - zadanie 6

--- lab06/lab06zad06.scala
+++ lab06/lab06zad06.scala
@@ -1,16 +1,19 @@
-// def freqMax[A](list: List[A]): (Set[A],Int) = {
-    
-// }
-
-@main def lab6zad6(): Unit = {
-    val x  = List(1, 1, 2, 4, 4, 4, 1, 3)
-    val a = x.map(n => (n, x.count(b => b == n)))
-    val maks = x.foldLeft(0) (
+def freqMax[A](list: List[A]): (Set[A],Int) = {
+    val elementIlosc = list.map(n => (n, list.count(b => b == n)))
+    val maks = list.foldLeft(0) (
         (acc, elem) => {
-            val tmp = x.count(n => n == elem)
+            val tmp = list.count(n => n == elem)
             if (tmp > acc) tmp
             else acc
         }
     )
-    
+    val filtered = elementIlosc.filter( (a, b) => b == maks)
+    val lista = filtered.map( (a, b) => a)
+    val zbior = lista.foldLeft(Set[A]()) ( (acc, elem) => acc + elem)
+    (zbior, maks)
+}
+
+@main def lab6zad6(): Unit = {
+    val l = List(1, 1, 2, 4, 4, 3, 4, 1, 3)
+    println(freqMax(l) == (Set(1, 4), 3))
 }
