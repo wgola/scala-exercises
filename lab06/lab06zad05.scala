842bccac038c06f926cd632e3e8a23916835f41b
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-13 11:19:23 +0200

    Laboratoria 6 - zadanie 5

---
+++ lab06/lab06zad05.scala
@@ -0,0 +1,19 @@
+import scala.annotation.tailrec
+
+def isOrdered[A](seq: Seq[A])(leq:(A, A) => Boolean): Boolean = {
+    val pary = seq.sliding(2, 1)
+    @tailrec
+    def helper(elementy: Iterator[Seq[A]], funkcja: (A, A) => Boolean, wynik: Boolean = true): Boolean = {
+        if (elementy.nonEmpty) {
+            val para = elementy.next
+            if (funkcja(para(0), para(1))) helper(elementy, funkcja, true)
+            else false 
+        }
+        else wynik
+    }
+    helper(pary, leq)
+}
+
+@main def lab6zad5(): Unit = {
+    println(isOrdered(Seq(1, 2, 2, 4))(_ <= _))
+}
