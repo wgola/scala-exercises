33c9154ced84ffecd6157a313ee649393e324add
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-27 11:18:02 +0200

    Laboratoria 7 - zadanie 2

---
+++ lab07/lab07zad02.scala
@@ -0,0 +1,15 @@
+def sumOpts(seq: Seq[Option[Double]]): Option[Double] = {
+    val wynik = seq.foldLeft(Option[Double](0.0))((acc, elem) => {
+        if (elem.nonEmpty) Option(elem.get + acc.get)
+        else acc
+    })
+    if (wynik.get == 0.0) None
+    else wynik
+}
+
+@main def lab7zad2(): Unit = {
+    val lista = List(Some(5.4), Some(-2.0), Some(1.0), None, Some(2.6))
+    println(sumOpts(lista) == Some(7.0))
+    println(sumOpts(List()) == None)
+    println(sumOpts(List(None, None)) == None)
+}
