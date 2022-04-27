e0392a8294851dcea9d41c8b76f4bd7131d3cdd0
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-27 11:47:28 +0200

    Laboratoria 7 - zadanie 4

---
+++ lab07/lab07zad04.scala
@@ -0,0 +1,10 @@
+def indices[A](l: List[A], el: A): Unit = {
+    val x = l.zipWithIndex
+    val y = x.groupMap(elem => elem(0))(elem => elem(1)).filter(elem => elem(0) == el)
+}
+
+@main def lab7zad4(): Unit = {
+    val lista = List(1, 2, 1, 1, 5)
+    println(indices(lista, 1))
+    println(indices(lista, 7))
+}
